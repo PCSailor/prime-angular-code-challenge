0187f3af9218e633406d4821252bb9168a657b53
Phil Curtis <Phil@PhilCurtis.io>
2017-03-13 14:19:58 -0500

    on DOM-work on Delete

--- server/public/scripts/controllers/hero-list.js
+++ server/public/scripts/controllers/hero-list.js
@@ -1,3 +1,28 @@
-app.controller('HeroListController', function(){
+app.controller('HeroListController', ['$http', function($http){
     console.log('Hero List Controller loaded');
-});
+      var self = this; // NOTE: self here = hlc in index.html
+      var heroList = { list: [] };
+      // self.newhero = {};  // NOTE: object connected to html form // user entry fields
+      self.heroList = heroList // NOTE: replaced by array AND this is the required code to bring in the full array
+
+      getHero();
+      function getHero() {
+        $http({
+          method: 'GET',
+          url: '/heroes'
+        }).then(function(response) {
+          console.log('response.data from factory: ', response.data);
+          self.herolist = response.data;  // NOTE: angJS adds alot with response alone so data pulls just result from server
+        });
+      }
+
+      self.deleteHero = function(heroToDelete){
+        $http({
+          method: 'DELETE',
+          url: '/heroes/delete/' + heroToDelete
+        }).then(function(response){
+          getHero();
+        });
+      }
+// NOTE: if it's tied to DOM, start with self.
+}]);
